ab887594ab7881b0d2d0c3dd4edff321a3b6d034
sanjanayadav07 <sanjanayadav3952@gmail.com>
2026-03-14 14:02:48 +0530

    update api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const API = axios.create({
+  baseURL: `${API_URL}/api`
+});
+
+/*
+import axios from "axios";
+
 export const API = axios.create({
   baseURL: "http://localhost:5000/api"
 });
+*/
